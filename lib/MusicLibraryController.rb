bda3e3ea76aaaa11aac7d35c9c24d2888478aba7
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-12-03 20:21:15 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -72,7 +72,7 @@ class MusicLibraryController
     input = " "
     input = gets.to_i
     if (1..Song.all.length).include?(input)
-      song = list_songs[input]
+      song = list_songs[input +=1]
       puts "Playing #{song.name} by #{song.artist.name}"
     #  binding.pry
     end
