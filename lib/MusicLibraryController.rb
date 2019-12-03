5c988503f86e5ecd97dd2f667a85acaa1c69fbeb
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-12-03 20:23:42 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -73,9 +73,9 @@ class MusicLibraryController
     input = gets.to_i
     if (1..Song.all.length).include?(input)
        song = Song.all.sort_by(&:name)[input - 1]
-      puts "Playing #{song.name} by #{song.artist.name}"
     #  binding.pry
     end
+    puts "Playing #{song.name} by #{song.artist.name}"
   end
 
 
