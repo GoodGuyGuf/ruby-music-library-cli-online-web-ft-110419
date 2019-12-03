e67911a9c552ab33721014143d3f64cc3d76072d
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-12-03 20:40:48 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -71,11 +71,10 @@ class MusicLibraryController
     list_songs = Song.all.sort{ |a, b| a.name <=> b.name }
     input = " "
     input = gets.to_i
-    if input <= list_songs.size && input > 0
-       song = Song.all.sort_by(&:name)[input - 1]
+    if (1..Song.all.length).include?(input)
+      song = list_songs[input+2]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
-  end
     #if (1..Song.all.length).include?(input)
     #  song = list_songs[input+2]
 end
